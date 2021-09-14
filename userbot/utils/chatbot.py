fb4a517baeef2edb08d777db6f388e21fb83342e
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-15 04:56:01 +0700

    Create chatbot.py

---
+++ userbot/utils/chatbot.py
@@ -0,0 +1,18 @@
+from subprocess import PIPE, Popen
+
+def install_pip(pipfile):
+    print(f"installing {pipfile}")
+    pip_cmd = ["pip", "install", f"{pipfile}"]
+    process = Popen(pip_cmd, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
+
+try:
+    import randomstuff
+except ModuleNotFoundError:
+    install_pip("randomstuff.py")
+    import randomstuff
+
+from userbot import RANDOM_STUFF_API_KEY
+
+rs_client = randomstuff.AsyncClient(api_key=RANDOM_STUFF_API_KEY, version="5")
