35e5ed3bee3bcaf4f4d4dda78c578e38d579bd76
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-10-01 22:12:13 +0700

    other: add limit info from spambot

--- userbot/modules/other.py
+++ userbot/modules/other.py
@@ -10,6 +10,7 @@ import os
 from asyncio import sleep
 
 from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.types import ChannelParticipantsKicked
 
@@ -153,6 +154,23 @@ async def _(event):
     )
 
 
+@register(outgoing=True, pattern=r"^\.limit(?: |$)(.*)")
+async def _(event):
+    await event.edit("`Processing...`")
+    async with bot.conversation("@SpamBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178220800)
+            )
+            await conv.send_message("/start")
+            response = await response
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit("**Mohon Unblock @SpamBot dan coba lagi**")
+            return
+        await event.edit(f"~ {response.message.message}")
+
+
 CMD_HELP.update(
     {
         "open": "**Plugin : **`open`\
@@ -210,6 +228,15 @@ CMD_HELP.update(
         "unbanall": "**Plugin : **`unbanall`\
         \n\n  •  **Syntax :** `.unbanall`\
         \n  •  **Function : **Untuk Menghapus Semua Pengguna yang dibanned di Daftar Banned GC.\
+    "
+    }
+)
+
+CMD_HELP.update(
+    {
+        "limit": "**Plugin : **`limit`\
+        \n\n  •  **Syntax :** `.limit`\
+        \n  •  **Function : **Untuk Mengecek akun anda sedang terkena limit atau tidak dengan menggunakan @spambot.\
     "
     }
 )
